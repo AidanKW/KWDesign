b9bc8c2fc8348d524a7baa574282d15c2a60ed24
AidanKW <akidderwolff@gmail.com>
2024-03-15 15:50:18 +0000

    testing server capabilities

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    // Handle GET requests here
+    res.end("Hello, World!");
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+});
