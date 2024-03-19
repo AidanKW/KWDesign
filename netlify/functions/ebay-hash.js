a669808d10af88dd3f6a61c69bdfc3e4eb610943
AidanKW <akidderwolff@gmail.com>
2024-03-19 08:17:33 +0000

    Create ebay-hash.js

---
+++ netlify/functions/ebay-hash.js
@@ -0,0 +1,29 @@
+// functions/ebay-hash.js
+const crypto = require('crypto');
+
+exports.handler = async (event, context) => {
+    try {
+        const { queryStringParameters } = event;
+        const { challenge_code } = queryStringParameters;
+
+        // My actual values on Netlify
+        const endpoint = process.env.EBAY_ENDPOINT; 
+        const yourVerificationToken = process.env.EBAY_VERIFICATION_TOKEN; 
+
+        const hash = crypto.createHash('sha256');
+        hash.update(challenge_code);
+        hash.update(yourVerificationToken);
+        hash.update(endpoint);
+        const responseHash = hash.digest('hex');
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ challengeResponse: responseHash }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal server error' }),
+        };
+    }
+};
